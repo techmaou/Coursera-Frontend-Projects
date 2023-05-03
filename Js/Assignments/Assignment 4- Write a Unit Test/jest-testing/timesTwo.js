58d210ee629e00cc171f2dd02e6fef5614bc6047
Maou <42063508+techmaou@users.noreply.github.com>
2023-05-03 15:57:50 +0530

    test exercise for JEST testing

---
+++ Js/Assignments/Assignment 4- Write a Unit Test/jest-testing/timesTwo.js
@@ -0,0 +1,7 @@
+// Task 1: Code the timesTwo function declaration
+function timesTwo(num) {
+    res = num * 2;
+    return res;
+}
+// Task 2: Export the timesTwo function as a module
+module.exports = timesTwo;
